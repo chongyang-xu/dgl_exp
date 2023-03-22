8e5376441d9a586e849596b29eb91420fb740277
Chongyang Xu <cxu@volta11.mpi-sws.org>
2023-03-22 13:00:24 +0100

    exp: gcn: update train_multi_machin

--- experiments/models_dgl_tensorflow/gcn/train_multi_machine.py
+++ experiments/models_dgl_tensorflow/gcn/train_multi_machine.py
@@ -1,5 +1,8 @@
 import argparse
 import time
+import json
+import os
+from multiprocessing import util
 
 import numpy as np
 import tensorflow as tf
@@ -8,6 +11,9 @@ from gcn import GCN
 import dgl
 from dgl.data import CiteseerGraphDataset, CoraGraphDataset, PubmedGraphDataset
 
+# data preparation
+# val set, test set
+# graphsage
 
 def evaluate(model, features, labels, mask):
     logits = model(features, training=False)
@@ -17,96 +23,106 @@ def evaluate(model, features, labels, mask):
     acc = tf.reduce_mean(tf.cast(indices == labels, dtype=tf.float32))
     return acc.numpy().item()
 
+# Checkpoint saving and restoring
 
-def main(args):
+def _is_chief(task_type, task_id, cluster_spec):
+    return (task_type is None
+            or task_type == 'chief'
+            or (task_type == 'worker'
+                and task_id == 0
+                and 'chief' not in cluster_spec.as_dict()))
+
+
+def _get_temp_dir(dirpath, task_id):
+    base_dirpath = 'workertemp_' + str(task_id)
+    temp_dir = os.path.join(dirpath, base_dirpath)
+    tf.io.gfile.makedirs(temp_dir)
+    return temp_dir
+
+
+def write_filepath(filepath, task_type, task_id, cluster_spec):
+    dirpath = os.path.dirname(filepath)
+    base = os.path.basename(filepath)
+    if not _is_chief(task_type, task_id, cluster_spec):
+        dirpath = _get_temp_dir(dirpath, task_id)
+    return os.path.join(dirpath, base)
+
+
+def get_train_strategy(args):
+    if args.n_worker == 1:
+        # reference https://www.tensorflow.org/tutorials/distribute/custom_training
+        train_strategy = tf.distribute.MirroredStrategy(
+            devices=["/gpu:0", "/gpu:1"],
+            cross_device_ops=tf.distribute.HierarchicalCopyAllReduce()
+        )
+    elif args.n_workser > 1:
+        # reference https://www.tensorflow.org/tutorials/distribute/multi_worker_with_ctl
+        os.environ["TF_CONFIG"] = json.dumps({
+            "cluster": {
+                "worker": ["host1:port", "host2:port"],
+            },
+            "task": {"type": "worker", "index": args.worker_idx}
+        })
+        communication_options = tf.distribute.experimental.CommunicationOptions(
+            implementation=tf.distribute.experimental.CommunicationImplementation.RING)  # RING(rpc), NCCL, AUTO
+        train_strategy = tf.distribute.MultiWorkerMirroredStrategy(
+            communication_options=communication_options)
+    else:
+        raise ValueError("Invalid worker num: {}".format(args.n_workser))
+
+
+def load_dataset(args):
     # load and preprocess dataset
     if args.dataset == "cora":
-        data = CoraGraphDataset()
+        dataset = CoraGraphDataset()
     elif args.dataset == "citeseer":
-        data = CiteseerGraphDataset()
+        dataset = CiteseerGraphDataset()
     elif args.dataset == "pubmed":
-        data = PubmedGraphDataset()
+        dataset = PubmedGraphDataset()
     else:
         raise ValueError("Unknown dataset: {}".format(args.dataset))
+    return dataset
 
-    # train_strategy = tf.distribute.MirroredStrategy()
-    train_strategy = tf.distribute.MirroredStrategy(
-        devices=["/gpu:0", "/gpu:1"],
-        cross_device_ops=tf.distribute.HierarchicalCopyAllReduce()
-    )
 
-    g = data[0]
-    if args.gpu < 0:
-        device = "/cpu:0"
-    else:
-        device = "/gpu:{}".format(args.gpu)
-        g = g.to(device)
+def main(args):
+    # set up parallel train strategy
+    train_strategy = get_train_strategy(args)
 
-    with tf.device(device):
-        features = g.ndata["feat"]
-        labels = g.ndata["label"]
-        train_mask = g.ndata["train_mask"]
-        val_mask = g.ndata["val_mask"]
-        test_mask = g.ndata["test_mask"]
-        in_feats = features.shape[1]
-        n_classes = data.num_labels
-        n_edges = data.graph.number_of_edges()
-        print(
-            """----Data statistics------'
-        #Edges %d
-        #Classes %d
-        #Train samples %d
-        #Val samples %d
-        #Test samples %d"""
-            % (
-                n_edges,
-                n_classes,
-                train_mask.numpy().sum(),
-                val_mask.numpy().sum(),
-                test_mask.numpy().sum(),
-            )
-        )
+    checkpoint_dir = os.path.join(util.get_temp_dir(), 'ckpt')
 
-        # add self loop
-        if args.self_loop:
-            g = dgl.remove_self_loop(g)
-            g = dgl.add_self_loop(g)
-        n_edges = g.number_of_edges()
-        # normalization
-        degs = tf.cast(tf.identity(g.in_degrees()), dtype=tf.float32)
-        norm = tf.math.pow(degs, -0.5)
-        norm = tf.where(tf.math.is_inf(norm), tf.zeros_like(norm), norm)
+    dataset = load_dataset(args)
+    train_dist_dataset = train_strategy.experimental_distribute_dataset(dataset)
+    test_dist_dataset = train_strategy.experimental_distribute_dataset(dataset)
 
-        g.ndata["norm"] = tf.expand_dims(norm, -1)
-
+    with train_strategy.scope():
         # create GCN model
         model = GCN(
-            g,
-            in_feats,
-            args.n_hidden,
-            n_classes,
-            args.n_layers,
-            tf.nn.relu,
-            args.dropout,
-        )
-
-        loss_fcn = tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True
-        )
+            g, in_feats, args.n_hidden, n_classes, args.n_layers, tf.nn.relu, args.dropout,)
         # use optimizer
         optimizer = tf.keras.optimizers.Adam(
-            learning_rate=args.lr, epsilon=1e-8
-        )
+            learning_rate=args.lr, epsilon=1e-8)
+        train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+            name='train_accuracy')
+        multi_worker_dataset = train_strategy.distribute_datasets_from_function(
+            dataset)
+    @tf.function
+    def val_step(iterator):
+        # https://www.tensorflow.org/tutorials/distribute/custom_training
+        def step_fn():
+            test_loss = []
+            test_accuracy = []
+        return train_strategy.run(step_fn, args=(next(iterator),))
 
-        # initialize graph
-        dur = []
-        for epoch in range(args.n_epochs):
-            if epoch >= 3:
-                t0 = time.time()
-            # forward
+    @tf.function
+    def train_step(iterator):
+        """Training step function."""
+
+        def step_fn(inputs):
+            features, labels, train_mask, val_mask = inputs
             with tf.GradientTape() as tape:
                 logits = model(features)
-                loss_value = loss_fcn(labels[train_mask], logits[train_mask])
+                loss_value = tf.keras.losses.SparseCategoricalCrossentropy(
+                    from_logits=True)(labels[train_mask], logits[train_mask])
                 # Manually Weight Decay
                 # We found Tensorflow has a different implementation on weight decay
                 # of Adam(W) optimizer with PyTorch. And this results in worse results.
@@ -115,27 +131,66 @@ def main(args):
                     loss_value = loss_value + args.weight_decay * tf.nn.l2_loss(
                         weight
                     )
-
                 grads = tape.gradient(loss_value, model.trainable_weights)
                 optimizer.apply_gradients(zip(grads, model.trainable_weights))
-            if epoch >= 3:
-                dur.append(time.time() - t0)
+                train_accuracy.update_state(
+                    labels[train_mask], logits[train_mask])
+                return loss_value
+        per_replica_losses = train_strategy.run(
+            step_fn, args=(next(iterator),))
+        return train_strategy.reduce(
+            tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
 
-            acc = evaluate(model, features, labels, val_mask)
-            print(
-                "Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f} | "
-                "ETputs(KTEPS) {:.2f}".format(
-                    epoch,
-                    np.mean(dur),
-                    loss_value.numpy().item(),
-                    acc,
-                    n_edges / np.mean(dur) / 1000,
-                )
-            )
+    epoch = tf.Variable(
+        initial_value=tf.constant(0, dtype=tf.dtypes.int64), name='epoch')
+    step_in_epoch = tf.Variable(
+        initial_value=tf.constant(0, dtype=tf.dtypes.int64),
+        name='step_in_epoch')
 
-        acc = evaluate(model, features, labels, test_mask)
-        print("Test Accuracy {:.4f}".format(acc))
+    # checkpoint
+    if args.use_checkpoint:
+        task_type, task_id, cluster_spec = (train_strategy.cluster_resolver.task_type,
+                                    train_strategy.cluster_resolver.task_id,
+                                    train_strategy.cluster_resolver.cluster_spec())
 
+        checkpoint = tf.train.Checkpoint(
+            model=model, epoch=epoch, step_in_epoch=step_in_epoch)
+        write_checkpoint_dir = write_filepath(
+            checkpoint_dir, task_type, task_id, cluster_spec)
+        checkpoint_manager = tf.train.CheckpointManager(
+            checkpoint, directory=write_checkpoint_dir, max_to_keep=1)
+        # Restoring the checkpoint
+        latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+        if latest_checkpoint:
+            checkpoint.restore(latest_checkpoint)
+
+    # train loop
+    n_epoch = args.n_epochs
+    step_per_epoch = args.num_steps_per_epoch
+    while epoch.numpy() < n_epoch:
+        iterator = iter(multi_worker_dataset)
+        total_loss = 0.0
+        num_batches = 0
+
+        while step_in_epoch.numpy() < step_per_epoch:
+            total_loss += train_step(iterator)
+            num_batches += 1
+            step_in_epoch.assign_add(1)
+        train_loss = total_loss / num_batches
+
+        #val_step(iterator)
+        print('Epoch: %d, accuracy: %f, train_loss: %f.'
+              % (epoch.numpy(), train_accuracy.result(), train_loss))
+
+        train_accuracy.reset_states()
+        # checkpoint
+        if args.use_checkpoint:
+            checkpoint_manager.save()
+            if not _is_chief(task_type, task_id, cluster_spec):
+                tf.io.gfile.rmtree(write_checkpoint_dir)
+
+        epoch.assign_add(1)
+        step_in_epoch.assign(0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="GCN")
@@ -154,6 +209,9 @@ if __name__ == "__main__":
         "--n-epochs", type=int, default=200, help="number of training epochs"
     )
     parser.add_argument(
+        "--batch-size-per-gpu", type=int, default=200, help="batch_size for 1 gpu, global batchsize will be calculated for dist training"
+    )
+    parser.add_argument(
         "--n-hidden", type=int, default=16, help="number of hidden gcn units"
     )
     parser.add_argument(
@@ -168,6 +226,12 @@ if __name__ == "__main__":
         help="graph self-loop (default=False)",
     )
     parser.set_defaults(self_loop=False)
+    parser.add_argument(
+        "--use-checkpoint",
+        action="store_true",
+        help="use checkpoint (default=False)",
+    )
+    parser.set_defaults(use_checkpoint=False)
     args = parser.parse_args()
     print(args)
 
