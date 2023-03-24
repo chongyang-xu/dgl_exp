eb9e4b9afa42d2f545a4298daac6ff999fd525a9
Chongyang Xu <cxu@volta12.mpi-sws.org>
2023-03-24 15:02:33 +0100

    exp: th: add infer_dist

---
+++ experiments/models_dgl_pytorch/infer_dist.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+import dgl
+import torch as th
+import tqdm
+
+
+def inference(model, g, x, batch_size, device):
+    """
+    Inference with the GraphSAGE model on full neighbors (i.e. without
+    neighbor sampling).
+
+    g : the entire graph.
+    x : the input of entire node set.
+
+    Distributed layer-wise inference.
+    """
+    # During inference with sampling, multi-layer blocks are very
+    # inefficient because lots of computations in the first few layers
+    # are repeated. Therefore, we compute the representation of all nodes
+    # layer by layer.  The nodes on each layer are of course splitted in
+    # batches.
+    # TODO: can we standardize this?
+    nodes = dgl.distributed.node_split(
+        np.arange(g.num_nodes()),
+        g.get_partition_book(),
+        force_even=True,
+    )
+    y = dgl.distributed.DistTensor(
+        (g.num_nodes(), model.n_hidden),
+        th.float32,
+        "h",
+        persistent=True,
+    )
+    for i, layer in enumerate(model.layers):
+        if i == len(model.layers) - 1:
+            y = dgl.distributed.DistTensor(
+                (g.num_nodes(), model.n_classes),
+                th.float32,
+                "h_last",
+                persistent=True,
+            )
+        print(
+            f"|V|={g.num_nodes()}, eval batch size: {batch_size}"
+        )
+
+        sampler = dgl.dataloading.NeighborSampler([-1])
+        dataloader = dgl.dataloading.DistNodeDataLoader(
+            g,
+            nodes,
+            sampler,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+        )
+
+        for input_nodes, output_nodes, blocks in tqdm.tqdm(dataloader):
+            block = blocks[0].to(device)
+            h = x[input_nodes].to(device)
+            h_dst = h[: block.number_of_dst_nodes()]
+            h = layer(block, (h, h_dst))
+            if i != len(model.layers) - 1:
+                h = model.activation(h)
+                h = model.dropout(h)
+
+            y[output_nodes] = h.cpu()
+
+        x = y
+        g.barrier()
+    return y
