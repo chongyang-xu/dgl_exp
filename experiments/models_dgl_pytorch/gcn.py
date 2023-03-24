3f9ad848ac77109c43ae934e68be45951eb65bfc
Chongyang Xu <cxu@volta12.mpi-sws.org>
2023-03-24 14:54:26 +0100

    exp: th: add gcn

---
+++ experiments/models_dgl_pytorch/gcn.py
@@ -0,0 +1,49 @@
+"""GCN using DGL nn package
+
+References:
+- Semi-Supervised Classification with Graph Convolutional Networks
+- Paper: https://arxiv.org/abs/1609.02907
+- Code: https://github.com/tkipf/gcn
+"""
+
+import dgl.nn.pytorch as dglnn
+import torch.nn as nn
+
+
+class GCN(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 activation,
+                 dropout):
+        super(GCN, self).__init__()
+        self.layers = nn.ModuleList()
+        # input layer
+        self.layers.append(
+            dglnn.GraphConv(in_feats, n_hidden, activation=activation, allow_zero_in_degree=True))
+        # hidden layers
+        for _ in range(1, n_layers - 1):
+            self.layers.append(
+                dglnn.GraphConv(n_hidden, n_hidden, activation=activation, allow_zero_in_degree=True))
+        # output layer
+        self.layers.append(
+            dglnn.GraphConv(n_hidden, n_classes, allow_zero_in_degree=True))
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, blocks, features):
+        h = features
+        for i, layer in enumerate(self.layers):
+            if i != 0:
+                h = self.dropout(h)
+            h = layer(blocks[i], h)
+        return h
+
+    def inference(self, g, x, batch_size, device):
+        pass
+
+    @contextmanager
+    def join(self):
+        """dummy join for standalone"""
+        yield
