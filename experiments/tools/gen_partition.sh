5936db81e905a46b0bf8cfd03aa6741f0cfba74b
Chongyang Xu <cxu@mpi-inf.mpg.de>
2023-05-17 14:55:03 +0200

    exp: tool: update gen_partition.sh

--- experiments/tools/gen_partition.sh
+++ experiments/tools/gen_partition.sh
@@ -8,3 +8,6 @@ PYTHON_CMD_PATH="python3"
 ${PYTHON_CMD_PATH} dist_pre.py --dataset ogbpa --data-root-path /workspace/work/ds4gnn/DATA --part-algo vcrandom    --n-parts 4 --num-hops 0
 ${PYTHON_CMD_PATH} dist_pre.py --dataset ogbpa --data-root-path /workspace/work/ds4gnn/DATA --part-algo vcoblivious --n-parts 4 --num-hops 0
 
+${PYTHON_CMD_PATH} dist_pre.py --dataset ogbpr --data-root-path /workspace/work/ds4gnn/DATA --part-algo vcrandom --n-parts 4 --num-hops 0 --self-loop
+${PYTHON_CMD_PATH} dist_pre.py --dataset ogbpr --data-root-path /workspace/work/ds4gnn/DATA --part-algo random --n-parts 4 --self-loop
+
