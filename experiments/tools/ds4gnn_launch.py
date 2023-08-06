95ed767ab8963fb9fb5a4c640769063bc7611085
Chongyang Xu <xu.chongyang@outlook.com>
2023-08-06 19:38:11 +0000

    exp: ds4gnn_launch: add use_first_n_parts

--- experiments/tools/ds4gnn_launch.py
+++ experiments/tools/ds4gnn_launch.py
@@ -13,6 +13,8 @@ parser.add_argument('--one_id', type=str, required=True, help='a unique id to id
 parser.add_argument('--dataset', type=str, required=True, choices=['ogbpr', 'ogbpa', 'cora'], help='')
 parser.add_argument('--part_algo', type=str, required=True, choices=['random', 'metis', 'vcrandom', 'vcoblivious', 'vcbgl', 'vcns', 'vcst', 'vcdeg', 'vcrgrp'], help='')
 parser.add_argument('--part_hop', type=int, required=True, choices=[0, 1], help='')
+parser.add_argument('--n_parts', type=int, required=True, help='')
+parser.add_argument('--use_first_n_parts', type=int, required=True, default=-1, help='')
 parser.add_argument('--model', type=str, required=True, choices=['sage', 'gcn'], help='')
 parser.add_argument('--layers', type=int, required=True, choices=[5, 4, 3, 2], help='')
 parser.add_argument('--batch_size', type=int, required=True, help='')
@@ -53,14 +55,16 @@ CLASS_NUM_OF = {
     'cora'  : 7
 }
 
-N_PARTS= args.n_mach * args.n_server_per_mach
+N_PARTS= args.n_parts
+FIRST_N= f"_first{args.use_first_n_parts}" if args.use_first_n_parts > 0 else ''
 
 IP_CONF="wtool/ipconfigs/{}.txt".format(args.ip_config)
-PART_CONF="DATA/ds_pre/{DS_NAME}/data_part_n{N_PARTS}_{PART_ALGO}_{PART_HOP}/{DS_NAME}.json".format(
+PART_CONF="DATA/ds_pre/{DS_NAME}/data_part_n{N_PARTS}_{PART_ALGO}_{PART_HOP}{FIRST_N}/{DS_NAME}.json".format(
 	DS_NAME=args.dataset,
     N_PARTS=N_PARTS,
     PART_ALGO=args.part_algo,
-    PART_HOP=args.part_hop)
+    PART_HOP=args.part_hop,
+    FIRST_N=FIRST_N)
 
 TRAIN_LAUNCHER="python3 /workspace/work/ds4gnn/compiling/dgl_dsg/tools/launch.py --workspace /workspace/work/ds4gnn"
 PYTHON_CMD_PATH="python3"
